a6c72c07d6c75f1f0ec4cf8cac777235e199f092
Artem Sokolov <sokolov.artem@hotmail.com>
2024-05-23 00:14:44 +0200

    Try load legacy font by default

--- Runtime/Scripts/Rendering/TextDrawer.cs
+++ Runtime/Scripts/Rendering/TextDrawer.cs
@@ -65,8 +65,14 @@ namespace Imui.Rendering
         public TextDrawer(MeshBuffer buffer)
         {
             this.buffer = buffer;
-        }
 
+            var defaultFont = new Font("LegacyRuntime");
+            if (defaultFont != null)
+            {
+                LoadFont(defaultFont, defaultFont.fontSize);
+            }
+        }
+        
         public void LoadFont(Font font, float? size = null)
         {
             UnloadFont();
