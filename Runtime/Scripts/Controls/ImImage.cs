13cf59592660f155582455125aedde7932574969
Artem Sokolov <sokolov.artem@hotmail.com>
2024-06-07 22:14:01 +0200

    Add image control

---
+++ Runtime/Scripts/Controls/ImImage.cs
@@ -0,0 +1,29 @@
+using Imui.Core;
+using Imui.Styling;
+using Imui.Utility;
+using UnityEngine;
+
+namespace Imui.Controls
+{
+    public static class ImImage
+    {
+        private static Vector4 ScaleOffset = new Vector4(1, 1, 0, 0);
+        
+        public static void Image(this ImGui gui, ImRect rect, Texture texture, bool preserveAspect = false)
+        {
+            if (gui.Canvas.Cull(rect))
+            {
+                return;
+            }
+            
+            if (preserveAspect)
+            {
+                rect = rect.WithAspect(texture.width / (float)texture.height);
+            }
+            
+            gui.Canvas.PushTexture(texture);
+            gui.Canvas.Rect(rect, ImColors.White, ScaleOffset);
+            gui.Canvas.PopTexture();
+        }
+    }
+}
