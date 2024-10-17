19e1a876c2356848960ee2fc8c9cf769b849cd18
Artem Sokolov <sokolov.artem@hotmail.com>
2024-10-18 01:00:03 +0200

    Fix debug code left

--- Runtime/Scripts/Rendering/ImMeshRenderer.cs
+++ Runtime/Scripts/Rendering/ImMeshRenderer.cs
@@ -99,7 +99,7 @@ namespace Imui.Rendering
                 properties.SetTexture(FontTexId, meshData.FontTex);
                 properties.SetFloat(InvColorMul, meshData.InvColorMul);
                 
-                if (false) // meshData.MaskRect.Enabled)
+                if (meshData.MaskRect.Enabled)
                 {
                     var radius = meshData.MaskRect.Radius * maskRadiusScale;
                     var rect = meshData.MaskRect.Rect;
@@ -116,7 +116,7 @@ namespace Imui.Rendering
                     properties.SetInteger(MaskEnabledId, 0);
                 }
                 
-                if (false) // meshData.ClipRect.Enabled)
+                if (meshData.ClipRect.Enabled)
                 {
                     var x = meshData.ClipRect.Rect.xMin * maskScale.x;
                     var y = meshData.ClipRect.Rect.yMin * maskScale.y;
