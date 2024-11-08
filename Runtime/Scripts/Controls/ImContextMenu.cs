f5943abf11ba99b8f15ca50a32f5547606308956
Artem Sokolov <sokolov.artem@hotmail.com>
2024-11-08 21:09:01 +0100

    Simple context menu

---
+++ Runtime/Scripts/Controls/ImContextMenu.cs
@@ -0,0 +1,58 @@
+using Imui.Core;
+using Imui.IO.Events;
+using UnityEngine;
+
+namespace Imui.Controls
+{
+    public struct ImContextMenuState
+    {
+        public bool Open;
+        public Vector2 Source;
+    }
+    
+    public static class ImContextMenu
+    {
+        public static bool BeginContextMenu(this ImGui gui, ImRect area)
+        {
+            var id = gui.GetNextControlId();
+            ref var state = ref gui.Storage.Get<ImContextMenuState>(id);
+            
+            gui.RegisterGroup(id, area);
+
+            ref readonly var evt = ref gui.Input.MouseEvent;
+            if (!state.Open && gui.IsGroupHovered(id) && evt is { Type: ImMouseEventType.Down, Button: 1 })
+            {
+                state.Open = true;
+                state.Source = gui.Input.MousePosition;
+                
+                gui.Input.UseMouseEvent();
+            }
+            
+            return BeginContextMenu(gui, id, state.Source, ref state.Open);
+        }
+        
+        public static bool BeginContextMenu(this ImGui gui, uint id, Vector2 source, ref bool open)
+        {
+            gui.PushId(id);
+            gui.Layout.Push(ImAxis.Vertical, new ImRect(source.x, source.y, 0, 0));
+
+            if (!gui.BeginMenu("context_menu", ref open))
+            {
+                gui.Layout.Pop();
+                gui.PopId();
+                
+                return false;
+            }
+
+            return true;
+        }
+
+        public static void EndContextMenu(this ImGui gui)
+        {
+            gui.EndMenu();
+            
+            gui.Layout.Pop();
+            gui.PopId();
+        }
+    }
+}
