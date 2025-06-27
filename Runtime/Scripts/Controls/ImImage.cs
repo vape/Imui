5e876f2469b18b549e7d554395435f63be7686e9
Artem Sokolov <sokolov.artem@hotmail.com>
2025-06-27 22:46:37 +0200

    Added support for sprites meshes

--- Runtime/Scripts/Controls/ImImage.cs
+++ Runtime/Scripts/Controls/ImImage.cs
@@ -1,6 +1,9 @@
+using System;
 using Imui.Core;
-using Imui.Style;
+using Imui.Rendering;
 using UnityEngine;
+using UnityEngine.Rendering;
+using UnityEngine.U2D;
 
 namespace Imui.Controls
 {
@@ -24,7 +27,7 @@ namespace Imui.Controls
 
         public static void Image(this ImGui gui, Texture texture, ImRect rect, bool preserveAspect = false)
         {
-            if (gui.Canvas.Cull(rect))
+            if (gui.Canvas.Cull(rect) || !texture)
             {
                 return;
             }
@@ -41,6 +44,99 @@ namespace Imui.Controls
             gui.Canvas.Rect(rect, White);
             gui.Canvas.SetTexScaleOffset(scaleOffset);
             gui.Canvas.PopTexture();
+        }
+
+        public static void Image(this ImGui gui, Sprite sprite, ImRect rect, bool preserveAspect = false)
+        {
+            if (gui.Canvas.Cull(rect) || !sprite.texture)
+            {
+                return;
+            }
+            
+            if (preserveAspect)
+            {
+                rect = rect.WithAspect(sprite.rect.width / sprite.rect.height);
+            }
+            
+            gui.Canvas.PushTexture(sprite.texture);
+            
+            if (sprite.packed && sprite.packingMode == SpritePackingMode.Tight)
+            {
+                DrawSpriteMesh(gui, sprite, rect);
+            }
+            else
+            {
+                var textureSize = new Vector2(sprite.texture.width, sprite.texture.height);
+                var textureRect = sprite.textureRect;
+                var textureRectOffset = sprite.textureRectOffset;
+                var spriteRect = sprite.rect;
+                var scaleOffset = new Vector4(textureRect.width / textureSize.x, textureRect.height / textureSize.y, textureRect.x / textureSize.x, textureRect.y / textureSize.y);
+                var scale = new Vector2(1.0f / (spriteRect.width / rect.W), 1.0f / (spriteRect.height / rect.H));
+                var posOffset = scale * textureRectOffset;
+                var sizeDelta = scale * (spriteRect.size - textureRect.size);
+                
+                rect.Position += posOffset;
+                rect.Size -= sizeDelta;
+                
+                var prevScaleOffset = gui.Canvas.GetTexScaleOffset();
+                gui.Canvas.SetTexScaleOffset(scaleOffset);
+                gui.Canvas.Rect(rect, White);
+                gui.Canvas.SetTexScaleOffset(prevScaleOffset);
+            }
+            
+            gui.Canvas.PopTexture();
+        }
+
+        // TODO (artem-s): at some point, should be moved to ImMeshDrawer
+        private static void DrawSpriteMesh(ImGui gui, Sprite sprite, ImRect rect)
+        {
+            var vertices = sprite.GetVertexAttribute<Vector3>(VertexAttribute.Position);
+            var indices = sprite.GetIndices();
+            var uvs = sprite.GetVertexAttribute<Vector2>(VertexAttribute.TexCoord0);
+
+#if UNITY_EDITOR
+            if (vertices.Length > 0)
+            {
+                try
+                {
+                    _ = vertices[0];
+                }
+                catch (ObjectDisposedException)
+                {
+                    // (artem-s): no other way to check whether underlying array is disposed, and that happens sometimes
+                    // when drawing in editor mode and using atlases
+                    return;
+                }
+            }
+#endif
+            
+            var buffer = gui.MeshDrawer.buffer;
+            var offset = rect.Position + (rect.Size * (sprite.pivot / sprite.rect.size));
+            var bounds = sprite.bounds.size;
+            
+            buffer.EnsureVerticesCapacity(buffer.VerticesCount + vertices.Length);
+            buffer.EnsureIndicesCapacity(buffer.IndicesCount + indices.Length);
+
+            for (int i = 0; i < vertices.Length; ++i)
+            {
+                var vertexPosition = vertices[i];
+                
+                ref var v = ref buffer.Vertices[buffer.VerticesCount + i];
+                v.Position.x = offset.x + (vertexPosition.x / bounds.x) * rect.W;
+                v.Position.y = offset.y + (vertexPosition.y / bounds.y) * rect.H;
+                v.Position.z = gui.MeshDrawer.Depth;
+                v.UV = uvs[i];
+                v.Color = White;
+                v.Atlas = gui.MeshDrawer.Atlas;
+            }
+
+            for (int i = 0; i < indices.Length; ++i)
+            {
+                buffer.Indices[buffer.IndicesCount + i] = indices[i] + buffer.VerticesCount;
+            }
+            
+            buffer.AddVertices(vertices.Length);
+            buffer.AddIndices(indices.Length);
         }
     }
 }
